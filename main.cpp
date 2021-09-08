88cfd1002dd28afc8138f52e9bb160e72e00c485
Sierra Brandt <44011600+SierraBrandt@users.noreply.github.com>
2021-09-08 17:00:34 +0000

    Initial commit

---
+++ main.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std ;
+
+int main() {
+      cout << "“The Old Pond” by Matsuo Bashō" << endl;
+
+      cout <<"An old silent pond\n"  
+       << "A frog jumps into the pond— \n"  
+       << "Splash! Silence again.\n" ; 
+}
